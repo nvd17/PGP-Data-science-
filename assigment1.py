5e7d74d3bac76fb3aec637395d5b091690324b39
nvd17 <50931336+nvd17@users.noreply.github.com>
2021-06-01 16:05:31 +0530

    Assignment 1

---
+++ assigment1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=2
+y=5
+xy='hey'
+print(x+y,xy)
+
+
+# In[5]:
+
+
+print(""" My name is naveed
+i love python""")
+
+
+# In[6]:
+
+
+string1='world'
+print("hello",string1)
+
+
+# In[7]:
+
+
+string2='!'
+print("hello",string1,string2)
+
+
+# In[9]:
+
+
+print(" jan \n Feb \n March \n April \n May")
+
+
+# In[10]:
+
+
+print("i want \\n to printed")
+
+
+# In[11]:
+
+
+x=10
+print(type(x))
+s="abc"
+print(type(s))
+value=True
+print(type(value))
+percent=90.00
+print(type(percent))
+
+
+# In[ ]:
+
+
+
+
